681583e7e393663df22e115d0c47c43f02877cae
jeffbramante-ops <jeffbramante@gmail.com>
2026-05-04 09:33:12 -0300

    v1

---
+++ seletor.py
@@ -0,0 +1,8 @@
+#--seletor de caracteres--
+letra = input("digite a letra")
+if letra == "a"
+   print("você escolheu a letra "a")
+elif letra =="b":
+   print("você escolheu a letra "b")
+else:
+   print("essa opção não disponivel")
