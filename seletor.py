9f659bbc692970eb1ee6a84a65fa6aeb380a74c4
jeffbramante-ops <jeffbramante@gmail.com>
2026-05-04 14:14:58 +0000

    v2

--- seletor.py
+++ seletor.py
@@ -1,8 +1,9 @@
-#--seletor de caracteres--
-letra = input("digite a letra")
-if letra == "a"
-   print("você escolheu a letra "a")
-elif letra =="b":
-   print("você escolheu a letra "b")
+print("sistema seletor de caracteres🔠")
+print("escolha uma letra e veja se esta disponivel")
+letra = input("digite a letra").upeer()
+if letra =="A":
+  print("você escolheu a letra A 👍")
+elif letra == 'B':
+  print("você escolheu a letra b 👍")
 else:
-   print("essa opção não disponivel")
+   print("essa opção não está disponivel ❌")
