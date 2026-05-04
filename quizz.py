824b701e992e429411ea6519619ae1ee2fe06d14
jeffbramante-ops <jeffbramante@gmail.com>
2026-05-04 13:04:18 -0300

    v3

---
+++ quizz.py
@@ -0,0 +1,6 @@
+#--quizz de espotes--
+print("1-a cada quantos anos a copa do mundo acontece?/A-a cada 5 anos/B-a cada 7 anos/C-a cada 4 anos")
+print("escolha uma letra e veja a resposta correta")
+letra = input("digite a letra que você deseja").upeer()
+if letra correta =="c"
+  print("você escolheu a letra c")
