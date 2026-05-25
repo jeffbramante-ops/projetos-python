3c4d84a73d6a1b0cd647c15cacb2da1a592890bb
jeffbramante-ops <jeffbramante@gmail.com>
2026-05-25 12:20:18 +0000

    v1

---
+++ cabecalho.py
@@ -0,0 +1,10 @@
+### definido e chamado a função
+def exibir_cabecalho():
+    print('=' * 30)
+    print('sistema de gestão escolar')
+    print('versão 1.0 = desenvolvimento no codespace')
+    print('=' * 30)
+
+### chamando a função
+exibir_cabecalho()
+print('menu principal:')
