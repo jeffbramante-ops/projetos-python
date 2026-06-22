4c16b4860e3804867a8f6e1fc918e911589bd8a3
jeffbramante-ops <jeffbramante@gmail.com>
2026-06-22 14:04:16 +0000

    v1

---
+++ podio.py
@@ -0,0 +1,4 @@
+podio = [" diego 1", "breno 2", "jp 3"]
+podio2 = int(input("digite o numero do aluno que desejar ver no podio (1-3): "))
+indice2 = podio2 - 1
+print(f"aluno no podio: {podio[indice2]}")
