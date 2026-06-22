f2fdd2926fbb04504142f4b853a015c82c2861c1
jeffbramante-ops <jeffbramante@gmail.com>
2026-06-22 12:28:28 +0000

    v1

---
+++ painel.py
@@ -0,0 +1,6 @@
+print("--- painel de atendimento ---")
+fila_espera = ["senha 01", "senha 02", "senha 03", "senha 04"]
+print(f"proximo atendimento{fila_espera[0]}")
+print(f"proximo atendimento{fila_espera[1]}")
+total = len(fila_espera)
+print(f"total aguardando: {total}")
